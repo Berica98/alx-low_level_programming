e3d85399d5f4bd9ba2a5a8d01325828680b00ba7
Berica98 <berica.george@gmail.com>
2023-09-07 21:57:22 -0700

    intel

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#inlude  < stdio.h >
+/**
+ * main - to print the size of data types
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+(
+char  (a);
+int (b);
+long int  (c);
+long long (d);
+float  (e);
+
+
+print ("Size of a char: %lu byte(s)\n", sizeof(a));
+print ("Size of a int: %lu byte(s)\n", sizeof(b));
+print ("Size of a long int: %lu byte(s)\n", sizeof(c));
+print ("Size of a long long int: %lu byte(s)\n", sizeof(d));
+print ("Size of a float: %lu byte(s)\n", sizeof(e));
+(return) (0)

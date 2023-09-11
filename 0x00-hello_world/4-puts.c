1e6b975add7b5d850faf8d36e2481f8da658f559
Berica98 <berica.george@gmail.com>
2023-09-11 11:51:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
 /**
-* main -it print out output using put
-*
-* Return: always(0)
+* main - A program that prints a line with puts function
+* Returns: % (Success)
 */
-'(' '|' int main(void)
-| (
-| puts("\"Programming is like building a multilingual puzzle")
-| return (0);
-(
-'(' '|' )
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

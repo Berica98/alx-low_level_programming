180baa733fe0e668dc8001dffd4f82a8e5ef7915
Berica98 <berica.george@gmail.com>
2023-09-11 18:27:15 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,12 @@
 #inlude  < stdio.h >
 /**
- * main - to print the size of data types
+ * main - This is the main  function.
  *
- * Return: always (0)
+ * Return: Always (0)
  *
  */
 int main(void)
-(
-char  (a);
-int (b);
-long int  (c);
-long long (d);
-float  (e);
-
-
-print ("Size of a char: %lu byte(s)\n", sizeof(a));
-print ("Size of a int: %lu byte(s)\n", sizeof(b));
-print ("Size of a long int: %lu byte(s)\n", sizeof(c));
-print ("Size of a long long int: %lu byte(s)\n", sizeof(d));
-print ("Size of a float: %lu byte(s)\n", sizeof(e));
-(return) (0)
+{
+	printf("with proper grammer, but the out come is a piece of art.");
+	return (0);
+}

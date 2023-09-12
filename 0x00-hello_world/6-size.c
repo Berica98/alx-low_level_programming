e5194057e39fa13158c703bf05e7a567005b60ee
Berica98 <berica.george@gmail.com>
2023-09-11 19:12:23 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	print ("Size of a char: %lu byte(s)\n", sizeof(char));
-	print ("Size of a int: %lu byte(s)\n", sizeof(int));
-	print ("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	print ("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	print ("Size of a float: %lu byte(s)\n", sizeof(float));
+	print ("Size of a char: %d byte(s)\n", sizeof(char));
+	print ("Size of a int: %d byte(s)\n", sizeof(int));
+	print ("Size of a long int: %d byte(s)\n", sizeof(long int));
+	print ("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	print ("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

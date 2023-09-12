d97489c1ac0427c2128803a57720d56471287e23
Berica98 <berica.george@gmail.com>
2023-09-11 19:00:44 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,23 @@
+#include <stdio.h>
 
+/**
+ * main - To print the size of data types
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char (a);
+int (b);
+long int  (c);
+long long (d);
+float  (e);
 
+print ("Size of a char: %lu byte(s)\n", sizeof(a));
+print ("Size of a int: %lu byte(s)\n", sizeof(b));
+print ("Size of a long int: %lu byte(s)\n", sizeof(c));
+print ("Size of a long long int: %lu byte(s)\n", sizeof(d));
+print ("Size of a float: %lu byte(s)\n", sizeof(e));
+return (0);
+}

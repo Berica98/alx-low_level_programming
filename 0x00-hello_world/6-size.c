343f52d0cfc02850d22ec1aa3c5aaa4e4fc2fda3
Berica98 <berica.george@gmail.com>
2023-09-11 19:07:28 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,10 @@
  */
 int main(void)
 {
-char (a);
-int (b);
-long int  (c);
-long long (d);
-float  (e);
-
-print ("Size of a char: %lu byte(s)\n", sizeof(a));
-print ("Size of a int: %lu byte(s)\n", sizeof(b));
-print ("Size of a long int: %lu byte(s)\n", sizeof(c));
-print ("Size of a long long int: %lu byte(s)\n", sizeof(d));
-print ("Size of a float: %lu byte(s)\n", sizeof(e));
-return (0);
+	print ("Size of a char: %lu byte(s)\n", sizeof(char));
+	print ("Size of a int: %lu byte(s)\n", sizeof(int));
+	print ("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	print ("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	print ("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }

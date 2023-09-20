0049ee23e48b6390f65958d7ed4519c096cc071b
Berica98 <berica.george@gmail.com>
2023-09-20 12:20:06 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
  * @src: Input value
  * Return: void
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src);
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')

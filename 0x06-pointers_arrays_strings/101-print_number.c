e9efe0f98d1536c124487446d8aac7b7b7d71822
Berica98 <berica.george@gmail.com>
2023-09-21 10:22:32 -0700

    number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,20 +3,21 @@
  * print_number - prints an integer
  * @n: input integer parameter
  *
- *
+ * Return: 0
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int n1;
 
+	n1 = n;
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
+		_putchar('-');
+		n1 = -n;
 	}
-	if (i / 10)
+	if (n1 / 10 != 0)
 	{
-		print_number(i / 10);
+		print_number(n1 / 10);
 	}
-		putchar(i % 10 + '0');
+		putchar((n1 % 10) + '0');
 }

29721614eb0b1013ec435ede8a6c5ec3dc42a558
Berica98 <berica.george@gmail.com>
2023-09-21 07:45:18 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *rot13 - Capitalize functions that encodes a string
+ * @str: The string to be capitalized
+ *
+ * Return: strings out
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char c[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char d[] = "NOPQRSTUVWZYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (str[i] == c[j])
+			{
+				str[i] = d[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
